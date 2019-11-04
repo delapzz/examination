7028b6915bbaba0898dfd9e8a49efa09ec376444
Joshua James Dela Cruz <joshuajamesddelacruz@yahoo.com.ph>
2019-11-05 00:10:19 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
 
-Route::get('/','ExpensesController@index');
+    Route::get('/','ExpensesController@index');
 
-Route::namespace('Admin')
-->prefix('admin')
-->name("admin.")
-->middleware("can:admin_manager")
-->group(function(){
-Route:: resource('/users', 'UsersController');
+    Route::namespace('Admin')
+        ->prefix('admin')
+        ->name("admin.")
+        ->middleware("can:admin_manager")
+        ->group(function(){
+            Route:: resource('/users', 'UsersController');
+    });
+
+    Route::namespace('ExpenseCategory')
+        ->prefix('expenseCategory')
+        ->name('expense.')
+        ->group(function(){
+            Route::resource('/expenses', 'ExpensesController');
+    });
+
 });
-
-
-Route::resource('/expenses', 'ExpensesController');
-
-});
-
-
